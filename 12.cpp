c59551d3d0e6024fcc1ad4c76b6ea368451bc4d2
Zhangtd <zhangtd95@outlook.com>
2018-11-29 20:57:56 +0800

    12-整数次幂

---
+++ 12.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    double Power(double base, int exponent) {
+    double res = 1;
+    int r = (exponent>0)? exponent: -exponent;
+    while(r !=  0)
+    {
+        if(r&1!=0)
+            res*=base;
+        base*=base;
+        r>>=1;
+    }
+    if(exponent<0)
+        res = 1/res;
+    return res;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    cout<<solution.Power(2, -2)<<endl;
+    return 0;
+}
