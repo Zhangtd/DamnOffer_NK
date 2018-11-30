e8aa009b50006dcbc1fb2282a04e2bc17cc9f870
Zhangtd <zhangtd95@outlook.com>
2018-11-30 21:48:06 +0800

    14-链表倒数第k个元素

---
+++ 14.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+class Solution {
+public:
+    ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+    int length = 0;
+    ListNode *res = pListHead;
+    if(res!= NULL)
+    {
+        length++;
+        while(res->next != NULL)
+        {
+            length++;
+            res = res->next;
+        }
+    }
+    res = pListHead;
+    for(int i=0; i< length-k; i++)
+    {
+        res = res->next;
+    }
+    return res;
+    }
+};
+
+int main()
+{
+    ListNode *head = new ListNode(0);
+    //head->val = 0;
+    ListNode *node = new ListNode(1);
+    //node->val = 1;
+    head->next = node;
+    for(int i=2; i<=10; i++)
+    {
+        ListNode *node1 = new ListNode(i);
+        //node1->val = i;
+        node->next = node1;
+        node = node1;
+    }
+
+    Solution solution;
+    cout<<solution.FindKthToTail(head, 1)->val;
+    return 0;
+}
