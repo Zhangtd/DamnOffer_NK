2c59c5aa4bea55f658d90cdebe78a1da8bb7c6a2
Zhangtd <zhangtd95@outlook.com>
2018-12-17 21:43:23 +0800

    52,53

---
+++ 52.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool match(char* str, char* pattern)
+    {
+        if(*str=='\0' && *pattern =='\0')
+            return true;
+        if(*str!='\0' && *pattern=='\0')
+            return false;
+        if(*(pattern+1)!='*')
+        {
+            if(*str==*pattern || (*str!='\0' && *pattern=='.'))
+                return match(str+1, pattern+1);
+            else
+                return false;
+        }
+        else
+        {
+            if(*str==*pattern || (*str!='\0' && *pattern=='.'))
+                return match(str, pattern+2) || match(str+1, pattern);
+            else
+                return match(str, pattern+2);
+        }
+    }
+};
+
+int main()
+{
+    char *s ="aaaa";
+    char *p ="a*a";
+    Solution solution;
+    cout<<solution.match(s, p);
+    return 0;
+}
